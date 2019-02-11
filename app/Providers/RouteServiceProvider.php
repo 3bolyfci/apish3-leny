5c82a61f9f892b7ee1b1ca87cfae5602c4a44630
Ahmed <ahmed.mostafa@code95.info>
2019-02-11 21:43:00 +0200

    init domain of project

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiAdminRoutes();
+        $this->mapApiJobSeekerRoutes();
+        $this->mapApiEmployerRoutes();
+    }
+
+
+    /**
+     * Define the "backend" routes for the application.
+     *
+     * These routes all receive api .
+     *
+     * @return void
+     */
+    protected function mapApiAdminRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->domain(env('Admin_url'))
+            ->namespace($this->namespace . '\Backend')
+            ->group(base_path('routes/admin.php'));
+    }
+
+    /**
+     * Define the "jobSeeker" routes for the application.
+     *
+     * These routes all receive api .
+     *
+     * @return void
+     */
+    protected function mapApiJobSeekerRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->domain(env('jobSeeker_url'))
+            ->namespace($this->namespace . '\JobSeeker')
+            ->group(base_path('routes/jobSeeker.php'));
+    }
+
+    /**
+     * Define the "Employer" routes for the application.
+     *
+     * These routes all receive api .
+     *
+     * @return void
+     */
+    protected function mapApiEmployerRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->domain(env('Employer_url'))
+            ->namespace($this->namespace . '\Employer')
+            ->group(base_path('routes/employer.php'));
+    }
+
+}
---
+++ database/migrations/2019_02_11_185031_create_job_seekers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobSeekersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_seekers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status'); // status of jobSeeker in system [seeker job job , employed ,student]
+            $table->text('bio')->nullable();  // Brief Biography
+            $table->string('position')->nullable(); // current position if jobSeeker working or not
+            $table->string('linked_in')->nullable(); // url of linkedIn if jobSeeker has
+            $table->integer('user_id')->unsigned(); // jobSeeker belong to one user in system
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_seekers');
+    }
+}
